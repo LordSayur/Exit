4267ad013040d90ef3322389db1e1c5bf7d6d1f7
Lord Sayur A <umar_317@hotmail.com>
2020-06-18 06:20:16 +0800

    Create core player movement logic.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Camera camera;
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        camera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude > Mathf.Epsilon)
+        {
+            float directionAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, directionAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, directionAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+}
