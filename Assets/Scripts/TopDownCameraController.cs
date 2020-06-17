c2a8e3fe5c0ba7ca3179a31405f2fc63c8d37a91
Lord Sayur A <umar_317@hotmail.com>
2020-06-18 06:20:38 +0800

    Create core camera logic.

---
+++ Assets/Scripts/TopDownCameraController.cs
@@ -0,0 +1,41 @@
+﻿using Cinemachine;
+using System.Collections;
+using UnityEngine;
+
+public class TopDownCameraController : MonoBehaviour
+{
+    private CinemachineFreeLook cinemachineFree;
+
+    void Start()
+    {
+        cinemachineFree = GetComponent<CinemachineFreeLook>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(RotateCamera(1));
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(RotateCamera(-1));
+        }
+    }
+
+    private IEnumerator RotateCamera(int direction)
+    {
+        float timer = 0;
+
+        float initialAngle = cinemachineFree.m_XAxis.Value;
+
+        while (timer <= 1.1f )
+        {
+            cinemachineFree.m_XAxis.Value = initialAngle;
+            cinemachineFree.m_XAxis.Value = Mathf.Lerp(cinemachineFree.m_XAxis.Value, cinemachineFree.m_XAxis.Value + 90 * direction, timer);
+            timer += 2f * Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
