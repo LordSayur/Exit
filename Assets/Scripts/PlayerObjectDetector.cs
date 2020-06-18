2862448752c0a91bdc3ab4d668b50b520671ed40
Lord Sayur A <umar_317@hotmail.com>
2020-06-19 06:43:15 +0800

    Implement highlight interact-able object

---
+++ Assets/Scripts/PlayerObjectDetector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerObjectDetector : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Interactable Object")
+        {
+            other.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Interactable Object")
+        {
+            other.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        }
+    }
+}
