ef2beac26cd7952ac7b6d805a874c2225a262f93
Lord Sayur A <umar_317@hotmail.com>
2020-06-20 09:46:50 +0800

    Implement open door on button click logic.

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private Animator doorAnimator;
+
+    private void Start()
+    {
+        doorAnimator = GetComponent<Animator>();   
+    }
+    public void OpenDoor()
+    {
+        doorAnimator.Play("Sliding");
+    }
+}
