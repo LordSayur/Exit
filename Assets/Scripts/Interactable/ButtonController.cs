c3ee4572dd13a996bd153a74e2ff6a054ee2255c
Lord Sayur A <umar_317@hotmail.com>
2020-06-20 09:46:16 +0800

    Implement button click logic.

---
+++ Assets/Scripts/Interactable/ButtonController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonController : MonoBehaviour, IInteractable
+{
+    public UnityEvent onButtonClicked;
+
+    public void Interact()
+    {
+        onButtonClicked.Invoke();
+    }
+}
